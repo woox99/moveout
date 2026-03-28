e68fd55a189f4291e9f7caeb81bf29cb53a3d9ac
Garett.Janulewicz <janulewiczgarett@gmail.com>
2026-03-28 12:03:25 -1000

    eleventy install

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("src/assets");
+
+    return {
+        dir: {
+            input: "src",
+            output: "dist",
+            includes: "_includes"
+        }
+    };
+
+};
