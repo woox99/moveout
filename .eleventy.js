b7368cd8209c01d40f31e484d20c2c053a464934
Garett.Janulewicz <janulewiczgarett@gmail.com>
2026-03-29 19:06:19 -1000

    sitemap.xml

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addPassthroughCopy("src/robots.txt");
+    eleventyConfig.addPassthroughCopy("src/sitemap.xml");
 
     return {
         dir: {
